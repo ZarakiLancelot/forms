1de50c3e12ab89473fe295a7df247325fce4cb60
EdwinEinsen <87045740+EdwinEinsen@users.noreply.github.com>
2021-09-22 19:53:08 -0600

    data binding in forms

---
+++ src/app/data/user-settings.ts
@@ -0,0 +1,10 @@
+export interface UserSettings {
+    name: string,
+    emailOffers: boolean,
+    interfaceStyle: string,
+    subscriptionType: string,
+    notes: string,
+    password: string,
+    date: string,
+    color: string
+}
--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'user-settings-form',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-settings-form.component.css']
 })
 export class UserSettingsFormComponent implements OnInit {
+
+  sourceUserSettings: UserSettings = {
+    name: 'Edwin',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'He aquí algunas notas...',
+    password: '',
+    color: '',
+    date: '',
+  };
+
+  userSettings: UserSettings = { ...this.sourceUserSettings };
 
   constructor() { }
 
