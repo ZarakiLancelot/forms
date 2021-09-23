8a96e668dbaba76e9e34f1330b6323c760d63b6f
EdwinEinsen <87045740+EdwinEinsen@users.noreply.github.com>
2021-09-23 03:09:24 -0600

    http form posting and data access

---
+++ src/app/data/data.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { UserSettings } from './user-settings';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  getSubscriptionTypes(): Observable<string[]> {
+    return of(['Monthly', 'Annual', 'Lifetime']);
+  }
+
+  postUserSettingsForm(userSettings: UserSettings): Observable<any> {
+    return this.http.post('https://enwznyptnmcnud9.m.pipedream.net', userSettings);
+    // return of(userSettings);
+  }
+}
